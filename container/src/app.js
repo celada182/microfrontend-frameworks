f453bf8b031b7d94f4298ccae76205c23cd64bc3
Javier Celada <jcelada@theworkshop.com>
2023-02-17 12:04:19 +0100

    Minor changes to trigger workflow

--- container/src/app.js
+++ container/src/app.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/marketing.app";
 
 export default () => {
   return <div>
-    <h1>Container Test</h1>
+    <h1>Container App</h1>
     <br/>
     <MarketingApp/>
   </div>
