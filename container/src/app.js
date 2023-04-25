923341427d3238e40f298b712968c9608d66b45f
Javier Celada <jcelada@theworkshop.com>
2023-04-25 11:05:19 +0200

    Generate class names for different proyects

--- container/src/app.js
+++ container/src/app.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import MarketingApp from "./components/marketing.app";
 import Header from "./components/Header";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core";
+
+// Only for material CSS class name generator
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
       <BrowserRouter>
-        <div>
-          <Header />
-          <MarketingApp/>
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header />
+            <MarketingApp/>
+          </div>
+        </StylesProvider>
       </BrowserRouter>
   );
 }
