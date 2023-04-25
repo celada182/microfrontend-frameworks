03bb7ca7b599b020a8d67e77218cc241edfedac9
Javier Celada <jcelada@theworkshop.com>
2023-04-25 15:28:32 +0200

    WIP: Router between projects

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # microfrontend-frameworks
 Micro frontend repository using different frameworks
+
+## Container
+Port 8080 (config/webpack.dev.js)
+> cd container
+> npm run start
+
+## Marketing
+Port 8081 (config/webpack.dev.js)
+> cd marketing
+> npm run start
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import { createMemoryHistory } from 'history';
 
-const mount = (element) => {
-  ReactDOM.render(<App />, element);
+const mount = (element, { onNavigate }) => {
+  const history = createMemoryHistory();
+  history.listen(onNavigate);
+  ReactDOM.render(<App history={history}/>, element);
 }
 
 if (process.env.NODE_ENV === 'development') {
