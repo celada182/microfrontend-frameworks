c8e9c6334cfebe5d9157b99fc9dab70eba49a92a
Javier Celada <jcelada@theworkshop.com>
2023-04-26 09:46:39 +0200

    Navigation between proyects

--- container/src/components/marketing.app.js
+++ container/src/components/marketing.app.js
@@ -1,14 +1,23 @@
 import React , {useRef, useEffect} from 'react';
 import {mount} from 'marketing/Marketing';
+import {useHistory} from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current, {
-      onNavigate: () => { console.log('Container navigation in Marketing')}
+    const {onParentNavigate} = mount(ref.current, {
+      onNavigate: ({pathname: nextPathname}) => {
+        if (history.location.pathname !== nextPathname){
+          history.push(nextPathname)
+        }
+      }
     });
-  });
+
+    if (onParentNavigate) history.listen(onParentNavigate);
+
+  }, []);  // Only run use effect when first render
 
   return <div ref={ref}/>
 }
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -5,14 +5,21 @@ import { createMemoryHistory } from 'history';
 
 const mount = (element, { onNavigate }) => {
   const history = createMemoryHistory();
-  history.listen(onNavigate);
+  if (onNavigate) history.listen(onNavigate);
   ReactDOM.render(<App history={history}/>, element);
+  return {
+    onParentNavigate({pathname: nextPathname}){
+      if (nextPathname !== history.location.pathname) {
+        history.push(nextPathname)
+      }
+    }
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
