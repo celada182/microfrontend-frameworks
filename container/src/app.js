154a6b1825bdb8edb0224cad2ad0f92800081f62
Javier Celada <jcelada@theworkshop.com>
2023-05-05 11:08:41 +0200

    Login redirect to dashboard

--- container/src/app.js
+++ container/src/app.js
@@ -1,8 +1,10 @@
-import React, {lazy, Suspense, useState} from 'react';
+import React, {lazy, Suspense, useState, useEffect} from 'react';
 import Header from "./components/Header";
 import {StylesProvider, createGenerateClassName} from "@material-ui/core";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Router, Route, Switch, Redirect} from "react-router-dom";
 import Progress from "./components/progress";
+import { createBrowserHistory } from 'history';
+import {Dashboard} from "@material-ui/icons";
 
 const MarketingLazy = lazy(() => import('./components/marketing.app'));
 const AuthLazy = lazy(() => import('./components/auth.app'));
@@ -13,10 +15,19 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn){
+      history.push('/dashboard');
+    }
+  }, [isSignedIn])
+
   return (
-      <BrowserRouter>
+      <Router history={history}>
         <StylesProvider generateClassName={generateClassName}>
           <div>
             <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn}/>
@@ -25,12 +36,15 @@ export default () => {
                 <Route path="/auth">
                   <AuthLazy onSignIn={() => setIsSignedIn(true)}></AuthLazy>
                 </Route>
-                <Route path="/dashboard" component={DashboardLazy}></Route>
+                <Route path="/dashboard">
+                  {!isSignedIn && <Redirect to='/'/>}
+                  <DashboardLazy></DashboardLazy>
+                </Route>
                 <Route path="/" component={MarketingLazy}></Route>
               </Switch>
             </Suspense>
           </div>
         </StylesProvider>
-      </BrowserRouter>
+      </Router>
   );
 }
